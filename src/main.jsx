a5841973a911f68af74a4eb8bcb9160c59cffb4f
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-12-05 22:56:06 -0500

    Update server URL

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import "./styles/index.css";
 
 // Set up the Apollo Client
 const httpLink = createHttpLink({
-  uri: "http://localhost:4001/graphql",
+  uri: "http://143.198.34.86:4001/graphql",
   credentials: "include",
 });
 
